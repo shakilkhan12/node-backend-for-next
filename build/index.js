7a4e8c5c1951bee2817569ddc4370d79f9e9213a
shakilkhan12 <shakilkhan621@gmail.com>
2022-08-03 06:13:15 +0500

    cors chnaged

--- build/index.js
+++ build/index.js
@@ -56,13 +56,11 @@ else if (process.env.NODE_ENV === "production") {
     console.log(process.env.LIVE_ORIGIN);
 }
 var app = (0, express_1.default)();
-app.use((0, cors_1.default)(
-//     {
-//     origin: process.env.NODE_ENV === 'production' ? process.env.LIVE_ORIGIN  : process.env.ORIGIN,
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-// }
-));
+app.use((0, cors_1.default)({
+    origin: process.env.NODE_ENV === 'production' ? process.env.LIVE_ORIGIN : process.env.ORIGIN,
+    credentials: true,
+    optionsSuccessStatus: 200,
+}));
 (0, db_1.default)();
 app.use(express_1.default.json());
 app.get('/test', function (req, res) { return console.log('test'); });
